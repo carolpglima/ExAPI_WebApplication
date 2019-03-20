9f388ecd4c550b9c2c698ca86d92b35baeb1f302
PugginaLima <Carolina_Puggina_lim@Dellteam.com>
2019-03-20 16:01:17 -0300

    Add project files.

---
+++ ExAPI_WebApplication/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExAPI_WebApplication.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+
+        public Cliente(string nome)
+        {
+            Nome = nome;
+            //ou this.Nome = nome;
+        }
+    }
+}
